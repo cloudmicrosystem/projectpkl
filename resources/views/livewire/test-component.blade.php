9af31575d68769b3b06fd9c317fe453fd05bbff1
Abyan Fawwaz <abyanfawwaz77@gmail.com>
2021-12-13 15:07:47 +0700

    Setup layouts and view again

---
+++ resources/views/livewire/test-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Ini Halaman Tes</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\TestComponent;
 
 // Route::get('/', function () {
 //     return view('layouts.base');
 // });
 
 Route::get('/', HomeComponent::class);
+Route::get('/test', TestComponent::class);
