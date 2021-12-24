f7074af04777c1e51fe34fbd1547a71a966fc681
Abyan Fawwaz <abyanfawwaz77@gmail.com>
2021-12-24 15:22:02 +0700

    Integrasi laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ArsipController;
 use App\Http\Controllers\JabatanController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,6 +21,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
 Route::resource('/kategori', KategoriController::class);
 Route::resource('/arsip', ArsipController::class);
 Route::resource('/jabatan', JabatanController::class);
+Route::resource('/user', UserController::class);
+// Route::resource('user', UserController::class);
