ed60f1872ba9528731d2653c0c1fcfd8de91c8bc
cloudmicrosystem <cloudmicrosystem@gmail.com>
2021-12-27 15:37:41 +0700

    add new content

---
+++ resources/views/content/arsip/list.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+@section('konten')
+<div class="row">
+    <table class="table table-responsive">
+        
+    <pre>
+        {{print_r($arsip)}}
+    </pre>
+</div>
+@endsection
