2b83595dcb4a698353dfb8e240c5454686806f5f
= <=>
2021-12-30 11:50:01 +0700

    menambahkan combobox pada user

--- resources/views/content/users/userCreate.blade.php
+++ resources/views/content/users/userCreate.blade.php
@@ -21,6 +21,12 @@
     </div>
   </div>
   <div class="form-group row">
+    <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Password</label>
+    <div class="col-sm-10">
+      <input type="password" name="in_password" class="form-control form-control-lg" id="colFormLabelLg" placeholder="masukkan password">
+    </div>
+</div>
+  <div class="form-group row">
     <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
     <div class="col-sm-10">
       <input type="email" name="in_email" class="form-control form-control-lg" id="colFormLabelLg" placeholder="masukkan alamat">
@@ -33,11 +39,19 @@
     </div>
   </div>
   <div class="form-group row">
-    <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Password</label>
+    <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Jabatan</label>
     <div class="col-sm-10">
-      <input type="password" name="in_password" class="form-control form-control-lg" id="colFormLabelLg" placeholder="masukkan password">
+      <select class="form-control">
+        <option>pilih jabatan</option>
+        <option>Sekretaris</option> 
+        <option>Bendahara</option>
+        <option>Kemasyarakatan</option>
+        <option>Kesehatan</option>
+        <option>Ketua Bidang</option>  
+      </select>
     </div>
   </div>
+</div>
   <button class="btn btn-primary" type="submit">Submit</button>
 </form>
 @endsection
