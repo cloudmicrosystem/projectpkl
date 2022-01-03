e8c8db37a8a7a769d803f080795f5eba41b8f516
= <=>
2022-01-03 12:14:07 +0700

    arsipView

--- resources/views/content/arsip/arsipView.blade.php
+++ resources/views/content/arsip/arsipView.blade.php
@@ -7,17 +7,14 @@
             <a href="{{ route('arsip.create') }}"><button class="btn btn-primary"><i class="fas fa-plus"></i> Tambah Data</button></a>
             </div>
         </div>
-        <thead>\
+        <thead>
             <tr>
                 <td>No</td>
-                <td>Nomor Arsip</td>
                 <td>Nama Arsip</td>
+                <td>Nomor Arsip</td>
                 <td>Kategori</td>
                 <td>Deskripsi</td>
-                <td>File Arsip</td>
-                <td>Diupload Oleh</td>
-                {{-- <td>Lihat</td> --}}
-                {{-- <td>Download</td> --}}
+                <td>Lihat</td>
                 <td>Action</td>
             </tr>
         </thead>
@@ -29,10 +26,7 @@
                 <td>{{ $arsp->no_arsip }}</td>
                 <td>{{ $arsp->nama_arsip }}</td>
                 <td>{{ $arsp->deskripsi }}</td>
-                <td>{{ $arsp->file_arsip }}</td>
-                <td><a href="{{url('/view', $arsp->id)}}">lihat</a></td>
-                <td><a href="{{url('/download', $arsp->file_arsip)}}">download</a></td>
-                <td>{{ $arsp->nama_user }}</td>
+                <td><a href="{{url('/download', $arsp->file_arsip)}}" target="_blank">lihat</a></td>
                 <td>
                     <a href="{{ route('arsip.edit', $arsp->id)}}"><i class="fas fa-edit"></i></a>
                     <form action="{{ route('arsip.destroy', $arsp->id) }}" method="POST">
--- resources/views/content/arsip/viewFile.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.base')
-@section('konten')
-    <iframe src="{{ Storage::get('arsip'); }}" frameborder="0"></iframe>
-    <p>Tes</p>
-@endsection
