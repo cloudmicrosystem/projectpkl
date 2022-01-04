4eab11d000c3601adb54540e77d34d522ea0469b
= <=>
2022-01-04 15:18:38 +0700

    galery view

--- resources/views/content/galeri/viewGaleri.blade.php
+++ resources/views/content/galeri/viewGaleri.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.base')
 @section('konten')
 <div class="row">
-@foreach($arsip as $ars)
 
+@foreach($arsip as $ars)
   <div class="col-sm-4">
     <div class="card" style="width: 18rem;">
-      {{-- @if ($ars->file_arsip == $arsip['0']->image) --}}
-      <img class="card-img-top" src="{{url('/storage/arsip/', $ars->file_arsip)}}" alt="Card image cap">
-      {{-- @elseif ($ars->file_arsip == $ars['0']->icon)
-      
-      @endif --}}
+      @if ($ars->file_arsip == $arsip['0']->file_arsip)
+      <embed class="card-img-top" src="{{url('/storage/arsip/', $ars->file_arsip)}}" width="700" height="300" >
+      @else
+      <embed class="fas fa-file-pdf" src="{{url('/storage/arsip/', $ars->file_arsip)}}" width="300" height="300" >
+      @endif
+     
       <div class="card-body">
         <h5 class="card-title">{{ $ars->nama_arsip}}</h5>
         <p class="card-text">{{ $ars->file_arsip}}</p>
@@ -19,5 +20,4 @@
   </div>
 @endforeach
 </div>
-
 @endsection
