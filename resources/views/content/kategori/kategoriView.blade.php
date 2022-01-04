81f56636df95c9cd4d7c7904e8568e1b996e8c3e
= <=>
2022-01-04 13:40:14 +0700

    alert kategori

--- resources/views/content/kategori/kategoriView.blade.php
+++ resources/views/content/kategori/kategoriView.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.base')
 @section('konten')
 <div class="card-body table-responsive">
-    @if (session('error'))
+    @if (session('message'))
      <div class="alert alert-danger alert-dismissible fade show" role="alert">
-          {{ session('error') }}
+          {{ session('message') }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
              <span aria-hidden="true">&times;</span>
          </button>
