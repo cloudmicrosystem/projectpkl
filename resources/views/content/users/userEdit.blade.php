d5686ff8ea995555646eb37332ec9699b9ba24d8
Abyan Fawwaz <abyanfawwaz77@gmail.com>
2022-01-05 13:24:11 +0700

    Update form edit user

--- resources/views/content/users/userEdit.blade.php
+++ resources/views/content/users/userEdit.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.base')
 @section('konten')
 <h3><p class="font-weight-bold">Edit User</p></h3><br>
-<form action="{{ route('user.edit', [$user['0']->id]) }}" method="POST">
-  @csrf
-  @method('PUT')
+<form action="{{ route('user.update', [$user['0']->id]) }}" method="POST">
+    @csrf
+    @method('PUT')
     <div class="form-group row">
         <label for="comFormLabellg" class="col-sm-2 col-form-label-lg font-weight-bold">Nama</label>
         <div class="col-sm-10">
@@ -19,7 +19,7 @@
     <div class="form-group row">
         <label for="comFormLabellg" class="col-sm-2 col-form-label-lg font-weight-bold">Password</label>
         <div class="col-sm-10">
-            <input type="password" name="password" class="form-control form-control-lg" id="colFromLabel1Lg" placeholder="edit password" value="{{ $user['0']->password }}">
+            <input type="password" name="password" class="form-control form-control-lg" id="colFromLabel1Lg" placeholder="edit password">
         </div>
     </div>
     <div class="form-group row">
@@ -36,14 +36,14 @@
     </div>
     <div class="form-group row">
     <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Jabatan</label>
-    <div class="col-sm-10" name="idJabatan" value="{{ $user['0']->id_jabatan }}">
-    <select class="form-control form-control-lg">
-        @foreach($jabatan as $jbt)
-          <option value="{{ $jbt->id }}">{{ $jbt->nama_jabatan}}</option>
-        @endforeach
-      </select>
+        <div class="col-sm-10" name="idJabatan" value="{{ $user['0']->id_jabatan }}">
+            <select class="form-control form-control-lg">
+                @foreach($jabatan as $jbt)
+                    <option value="{{ $jbt->id }}">{{ $jbt->nama_jabatan}}</option>
+                @endforeach
+            </select>
+        </div>
     </div>
-  </div>
-  <button class="btn btn-primary" type="submit">Simpan</button>
+    <button class="btn btn-primary" type="submit">Simpan</button>
 </form>
 @endsection
