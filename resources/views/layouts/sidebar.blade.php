9ed1ae4ee161b047c65e78679ab7b5d1fca29dc8
Abyan Fawwaz <abyanfawwaz77@gmail.com>
2022-01-23 19:02:07 +0700

    Update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,97 +3,102 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
         <div class="sidebar-brand-icon rotate-n-15">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-file-earmark-medical" viewBox="0 0 16 16">
-            <path d="M7.5 5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L6 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L8 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317V5.5zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
-            <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                class="bi bi-file-earmark-medical" viewBox="0 0 16 16">
+                <path
+                    d="M7.5 5.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L6 7l-.549.317a.5.5 0 1 0 .5.866l.549-.317V8.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L8 7l.549-.317a.5.5 0 1 0-.5-.866l-.549.317V5.5zm-2 4.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z" />
+                <path
+                    d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
             </svg>
         </div>
         <div class="sidebar-brand-text mx-3">E-Arsip</div>
     </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+    @if (Auth::user()->id_jabatan != null || Auth::user()->level == 'admin')
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="/">
-            <i class="fas fa-fw fa-home"></i>
-            <span>Dashboard</span></a>
-    </li>
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="/">
+                <i class="fas fa-fw fa-home"></i>
+                <span>Dashboard</span></a>
+        </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+        <!-- Divider -->
+        <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Interface
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link" href="/arsip">
-            <i class="fas fa-fw fa-archive"></i>
-            <span>Arsip</span></a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#disposisiPages"
-            aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Disposisi</span>
-        </a>
-        <div id="disposisiPages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="{{ route('disposisi.index') }}">Pengajuan Surat</a>
-                <a class="collapse-item" href="{{ route('disposisiPenerima') }}">Surat Diterima</a>
-            </div>
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Interface
         </div>
-    </li>
 
-    @if (Auth::user()->level == 'admin')
-    <li class="nav-item">
-        <a class="nav-link" href="/kategori">
-            <i class="fas fa-fw fa-mail-bulk"></i>
-            <span>Kategori</span></a>
-    </li>
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link" href="/arsip">
+                <i class="fas fa-fw fa-archive"></i>
+                <span>Arsip</span></a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#disposisiPages"
+                aria-expanded="true" aria-controls="collapsePages">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Disposisi</span>
+            </a>
+            <div id="disposisiPages" class="collapse" aria-labelledby="headingPages"
+                data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="{{ route('disposisi.index') }}">Pengajuan Surat</a>
+                    <a class="collapse-item" href="{{ route('disposisiPenerima') }}">Surat Diterima</a>
+                </div>
+            </div>
+        </li>
+
+        @if (Auth::user()->level == 'admin')
+            <li class="nav-item">
+                <a class="nav-link" href="/kategori">
+                    <i class="fas fa-fw fa-mail-bulk"></i>
+                    <span>Kategori</span></a>
+            </li>
+        @endif
+
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            User
+        </div>
+
+        @if (Auth::user()->level == 'admin')
+            <li class="nav-item">
+                <a class="nav-link" href="/user">
+                    <i class="fas fa-fw fa-user"></i>
+                    <span>List user</span></a>
+            </li>
+        @endif
+
+        @if (Auth::user()->level == 'admin')
+            <li class="nav-item">
+                <a class="nav-link" href="/jabatan">
+                    <i class="fas fa-fw fa-user-tie"></i>
+                    <span>Jabatan</span></a>
+            </li>
+        @endif
+
+        <li class="nav-item">
+            <a class="nav-link" href="/galeri">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Galeri</span></a>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
     @endif
-
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
-
-    @if (Auth::user()->level == 'admin')
-    <li class="nav-item">
-        <a class="nav-link" href="/user">
-            <i class="fas fa-fw fa-user"></i>
-            <span>List user</span></a>
-    </li>
-    @endif
-
-    @if (Auth::user()->level == 'admin')
-    <li class="nav-item">
-        <a class="nav-link" href="/jabatan">
-            <i class="fas fa-fw fa-user-tie"></i>
-            <span>Jabatan</span></a>
-    </li>
-    @endif
-
-    <li class="nav-item">
-        <a class="nav-link" href="/galeri">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Galeri</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
 </ul>
